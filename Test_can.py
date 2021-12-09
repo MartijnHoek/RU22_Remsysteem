52226e5c7deca6c2b7c23d96a63ab960d7e7cbb8
MartijnHoek <martynhoek@gmail.com>
2021-12-09 09:14:24 +0100

    "Test"

--- Test_can.py
+++ Test_can.py
@@ -1,8 +1,8 @@
 import can
 
 bus = can.interface.Bus(channel='can0', bustype='socketcan_native')
-msg = can.Message(arbitration_id=0x502, data=[6, 17], extended_id=False)
-bus.send(msg)
+#msg = can.Message(arbitration_id=0x502, data=[6, 17], extended_id=False)
+#bus.send(msg)
 
-#notifier = can.Notifier(bus, [can.Printer()])
+notifier = can.Notifier(bus, [can.Printer()])
 
