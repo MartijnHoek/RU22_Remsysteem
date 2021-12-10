ccbc5c924dfceb7d20b299110ad5beb502e3c124
MartijnHoek <martynhoek@gmail.com>
2021-12-10 11:47:23 +0100

    Verschil gemaakt tussen CAN ontvangen en verzenden

--- CAN_Recieve.py
+++ CAN_Recieve.py
@@ -1,8 +1,13 @@
 import can
+import time
 
 bus = can.interface.Bus(channel='can0', bustype='socketcan_native')
 #msg = can.Message(arbitration_id=0x502, data=[6, 17], extended_id=False)
 #bus.send(msg)
 
-notifier = can.Notifier(bus, [can.Printer()])
+#notifier = can.Notifier(bus, [can.Printer()])
 
+while True:
+    notifier = can.Notifier(bus, [can.Printer()])
+    time.sleep(1000)
+    
---
+++ CAN_Send.py
@@ -0,0 +1,14 @@
+import can
+import time
+
+bus = can.interface.Bus(channel='can0', bustype='socketcan_native')
+#msg = can.Message(arbitration_id=0x502, data=[6, 17], extended_id=False)
+#bus.send(msg)
+
+#notifier = can.Notifier(bus, [can.Printer()])
+
+while True:
+    msg = can.Message(arbitration_id=0x502, data=[6, 17], extended_id=False)
+    bus.send(msg)
+    time.sleep(1000)
+    
