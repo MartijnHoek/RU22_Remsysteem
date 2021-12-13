8ac2780abbe008b4058df8d395dc85236bcf0d6b
MartijnHoek <martynhoek@gmail.com>
2021-12-13 10:30:09 +0100

    Andere ontvang code

--- CAN_Recieve.py
+++ CAN_Recieve.py
@@ -10,6 +10,5 @@ bus = can.interface.Bus(channel='can0', bustype='socketcan_native')
 #notifier = can.Notifier(bus, [can.Printer()])
 
 while True:
-    notifier = can.Notifier(bus, [can.Printer()])
-    time.sleep(1)
-    
+    message = bus.recv()
+    print(message)
