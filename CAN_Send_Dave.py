719bc8ede95aca098128fdeb78dc1036927b3178
MartijnHoek <martynhoek@gmail.com>
2021-12-16 15:42:09 +0100

    CAN opzet overgezet naar Dave's CAN en Dave's RPi

--- CAN_Send_Dave.py
+++ CAN_Send_Dave.py
@@ -5,9 +5,9 @@ import cantools
 import can
 from can.message import Message
 
-db = cantools.db.load_file('/home/pi/RU22_Remsysteem/Test_DBC_Files.dbc')
-msg = db.get_message_by_name('Test_berichten')
 bus = can.interface.Bus(channel='can0', bustype='socketcan_native')
+db = cantools.db.load_file('/home/pi/RU22_Remsysteem/Test_DBC_Files.dbc')
+Test_berichten = db.get_message_by_name('Test_berichten')
 
 def arduino_map(x, in_min, in_max, out_min, out_max):
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
@@ -17,9 +17,19 @@ while True:
     Sensor1Stand = arduino_map(3, 0, 3.3, 0, 255)
     Sensor2Stand = arduino_map(3, 0, 3.3, 0, 255)
 
-    msg_data = msg.encode({'Pot1':Sensor1Stand, 'Pot2':Sensor2Stand})
-    bericht=can.Message(arbitration_id=msg.frame_id, data=msg_data)
-    bus.send(bericht)
+    Test_berichten_data = Test_berichten.encode({'Pot1':Sensor1Stand, 'Pot2':Sensor2Stand})
+    Test_berichten_bericht=can.Message(arbitration_id=Test_berichten.frame_id, data=Test_berichten_data)
+    bus.send(Test_berichten_bericht)
     
     message = bus.recv()
-    print(db.decode_message(message.arbitration_id, message.data)) 
+    message=db.decode_message(message.arbitration_id, message.data) 
+    Test_berichten_Pot1=message.get('Pot1')
+    Test_berichten_Pot2=message.get('Pot2')
+    
+    print(Test_berichten_Pot1)
+    print(Test_berichten_Pot2)
+    
+    if(Test_berichten_Pot1 == 232):
+        print('werkt')
+    if(Test_berichten_Pot2 <= 200):
+        print('owjo') 
